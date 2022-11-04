28d16b9644eb8f99daa87285185d5618097d1f1a
Aleksandr Gordeev <aleksandr@cloudcannon.com>
2022-11-04 22:17:57 +0400

    Configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,14 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+  plugins: ["react", "jest"],
+  rules: {},
 };
