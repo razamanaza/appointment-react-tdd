db6c3339b3ebcedf4155bae871de75060cabc75b
Aleksandr Gordeev <aleksandr@cloudcannon.com>
2022-11-04 23:19:52 +0400

    Refactor

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -4,25 +4,29 @@ import { act } from 'react-dom/test-utils';
 import { Appointment } from '../src/Appointment';
 
 describe('Appointment', () => {
-  it('renders the customer first name', () => {
-    const customer = { firstName: 'Ashley' };
-    const component = <Appointment customer={customer} />;
-    const container = document.createElement('div');
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement('div');
     document.body.replaceChildren(container);
+  });
+
+  const render = (component) =>
     act(() => {
       ReactDOM.createRoot(container).render(component);
     });
+
+  it('renders the customer first name', () => {
+    const customer = { firstName: 'Ashley' };
+    const component = <Appointment customer={customer} />;
+    render(component);
     expect(document.body.textContent).toContain('Ashley');
   });
 
   it('renders another customer first name', () => {
     const customer = { firstName: 'Jordan' };
     const component = <Appointment customer={customer} />;
-    const container = document.createElement('div');
-    document.body.replaceChildren(container);
-    act(() => {
-      ReactDOM.createRoot(container).render(component);
-    });
+    render(component);
     expect(document.body.textContent).toContain('Jordan');
   });
 });
