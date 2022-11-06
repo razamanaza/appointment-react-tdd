44530b13ef3c85b9dbb264f8917b008e6466245f
Aleksandr Gordeev <aleksandr@cloudcannon.com>
2022-11-06 13:59:02 +0400

    Add test for empty appointments view div

--- src/Appointment.js
+++ src/Appointment.js
@@ -3,3 +3,7 @@ import React from 'react';
 export function Appointment({ customer }) {
   return <div>{customer.firstName}</div>;
 }
+
+export function AppointmentsDayView() {
+  return <div id="appointmentsDayView" />;
+}
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { act } from 'react-dom/test-utils';
-import { Appointment } from '../src/Appointment';
+import { Appointment, AppointmentsDayView } from '../src/Appointment';
 
 describe('Appointment', () => {
   let container;
@@ -18,15 +18,32 @@ describe('Appointment', () => {
 
   it('renders the customer first name', () => {
     const customer = { firstName: 'Ashley' };
-    const component = <Appointment customer={customer} />;
-    render(component);
+    render(<Appointment customer={customer} />);
     expect(document.body.textContent).toContain('Ashley');
   });
 
   it('renders another customer first name', () => {
     const customer = { firstName: 'Jordan' };
-    const component = <Appointment customer={customer} />;
-    render(component);
+    render(<Appointment customer={customer} />);
     expect(document.body.textContent).toContain('Jordan');
+  });
+});
+
+describe('AppointmentsDayView', () => {
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component) =>
+    act(() => {
+      ReactDOM.createRoot(container).render(component);
+    });
+
+  it('renders a div with the right id', () => {
+    render(<AppointmentsDayView appointments={[]} />);
+    expect(document.querySelector('div#appointmentsDayView')).not.toBeNull();
   });
 });
